a11d7560498850072d38eb9e46fc35649de9beeb
August.zuo <leftstick@sohu.com>
2011-09-25 21:14:56 +0800

    update import and export 2

--- Password/src/org/nanfeng/util/DataOperator.java
+++ Password/src/org/nanfeng/util/DataOperator.java
@@ -3,7 +3,13 @@ package org.nanfeng.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.sf.json.JSONObject;
 
 import org.apache.commons.io.FileUtils;
 import org.nanfeng.bean.impl.ObjectInfo;
@@ -15,42 +21,33 @@ import org.nanfeng.dao.impl.ObjectInfoDaoImpl;
 public class DataOperator {
 	private String userName;
 	private ObjectInfoDao objectinfodao;
-	private String split_0x0f;
-	private String split_0x00;
-	private String split_0x01;
+
+	private String OBJ_NAME = "object_name";
+	private String OBJ_DESC = "object_description";
+	private String OBJ_PROP = "object_properties";
 
 	public DataOperator(String user_name) {
 		userName = user_name;
 		objectinfodao = new ObjectInfoDaoImpl();
-		split_0x0f = new String(new byte[] { 0x0f });
-		split_0x00 = new String(new byte[] { 0x00 });
-		split_0x01 = new String(new byte[] { 0x01 });
 	}
 
 	public void exportData(String exportPath) throws IOException {
 		File export = new File(exportPath);
 		List<String> list = new ArrayList<String>();
 		List<ObjectInfo> datas = objectinfodao.get(userName);
-		StringBuffer sb = new StringBuffer();
 		for (ObjectInfo obj : datas) {
-			sb.delete(0, sb.length());
-			sb.append(obj.getObject_name());
-			sb.append(split_0x0f);
-			sb.append(obj.getObject_description());
-			sb.append(split_0x0f);
-			sb.append("{");
-			List<ObjectProperty> objectProperties = obj.getObjectProperties();
-			for (int i = 0; i < objectProperties.size(); i++) {
-				if (i != 0)
-					sb.append(split_0x00);
-				sb.append("(");
-				sb.append(objectProperties.get(i).key + split_0x01
-						+ objectProperties.get(i).value);
-				sb.append(")");
+			Map<String, Object> objectinfo = new LinkedHashMap<String, Object>();
+			objectinfo.put(OBJ_NAME, obj.getObject_name());
+			objectinfo.put(OBJ_DESC, obj.getObject_description());
+			List<ObjectProperty> pro = obj.getObjectProperties();
+			Map<String, String> pros = new LinkedHashMap<String, String>();
+			for (ObjectProperty op : pro) {
+				pros.put(op.key, op.value);
 			}
-			sb.append("}");
-			list.add(sb.toString());
+			objectinfo.put(OBJ_PROP, pros);
+			list.add(JSONObject.fromObject(objectinfo).toString());
 		}
+
 		FileUtils.writeLines(export, "GBK", list);
 	}
 
@@ -59,15 +56,27 @@ public class DataOperator {
 		File importf = new File(importPath);
 		List<String> list = FileUtils.readLines(importf, "GBK");
 		ObjectInfo object = null;
+		JSONObject json = null;
 		int total = 0;
 		int success = 0;
 		for (String line : list) {
 			total++;
 			ObjectInfo obj = new ObjectInfo();
 			try {
+				json = JSONObject.fromObject(line);
 				obj.setUser_name(user);
-				analyze(obj, line);
-			} catch (RuntimeException e) {
+				obj.setObject_name(json.getString(OBJ_NAME));
+				obj.setObject_description(json.getString(OBJ_DESC));
+				@SuppressWarnings("unchecked")
+				Map<String, String> pros = (Map<String, String>) json
+						.get(OBJ_PROP);
+				for (Iterator<Entry<String, String>> iterator = pros.entrySet()
+						.iterator(); iterator.hasNext();) {
+					Entry<String, String> entry = iterator.next();
+					obj.addProperty(entry.getKey(), entry.getValue());
+
+				}
+			} catch (Exception e) {
 				continue;
 			}
 			object = objectinfodao.get(user, obj.getObject_name());
@@ -86,34 +95,5 @@ public class DataOperator {
 		result[0] = total;
 		result[1] = success;
 		return result;
-	}
-
-	private void analyze(ObjectInfo obj, String line) {
-		String[] element = line.split(split_0x0f);
-		if (element.length != 3)
-			throw new RuntimeException();
-		obj.setObject_name(element[0]);
-		obj.setObject_description(element[1]);
-		String end = element[2];
-		if (end.charAt(0) != '{')
-			throw new RuntimeException();
-		if (end.charAt(end.toCharArray().length - 1) != '}')
-			throw new RuntimeException();
-		List<ObjectProperty> list = new ArrayList<ObjectProperty>();
-		String content = end.substring(1, end.length() - 1);
-		String[] properties = content.split(split_0x00);
-		for (String property : properties) {
-			if (property.charAt(0) != '(')
-				throw new RuntimeException();
-			if (property.charAt(property.toCharArray().length - 1) != ')')
-				throw new RuntimeException();
-			String[] entry = property.substring(1, property.length() - 1)
-					.split(split_0x01);
-			if (entry.length != 2)
-				throw new RuntimeException();
-			ObjectProperty op = new ObjectProperty(entry[0], entry[1]);
-			list.add(op);
-		}
-		obj.setObjectProperties(list);
 	}
 }
